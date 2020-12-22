b7b2aa4fcfaebbe6cf79764f13ee35b968cd7622
akhalizev <ahalizev@gmail.com>
2020-12-22 00:43:29 -0600

    Book component rewrite

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDom from "react-dom";
 import './index.css';
 
+//setup vars
+// const Image = () => <img src="https://cdn.builtinaustin.com/cdn-cgi/image/f=auto,fit=crop,sharpen=.3,w=96,h=96/sites/www.builtinaustin.com/files/2020-11/Logo_Mark_Full_Color_Web_0.png" alt="" />
+// const Title = () => <h1>This is a title</h1>
+// const Author = () => <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop:'0.25rem', textTransform:'uppercase'}}>Author of this book</h4> //inline style
+
+const img = "https://cdn.builtinaustin.com/cdn-cgi/image/f=auto,fit=crop,sharpen=.3,w=96,h=96/sites/www.builtinaustin.com/files/2020-11/Logo_Mark_Full_Color_Web_0.png"
+const author = "Amelia Hepworth"
+const title = "I love you to the Moon and Back"
+
 //components are going here, in this App called Greeting
 function BookList() {
   return (
@@ -16,16 +25,20 @@ function BookList() {
 
 const Book = () => {
   return <article className='book'>
-    <Image />
+    {/* <Image />
     <Title />
-    <Author/>
+    <Author /> */}
+    <img src={img} alt="" />
+    <h1>{author}</h1>
+    <h4>{title}</h4>
   </article>
 }
 
-const Image = () => <img src="https://cdn.builtinaustin.com/cdn-cgi/image/f=auto,fit=crop,sharpen=.3,w=96,h=96/sites/www.builtinaustin.com/files/2020-11/Logo_Mark_Full_Color_Web_0.png" alt="" />
 
-const Title = () => <h1>This is a title</h1>
-const Author = () => <h2>Author of this book</h2>
+
+
+
+
 // const Greeting = () => {
 //   return React.createElement(
 //     "div",
