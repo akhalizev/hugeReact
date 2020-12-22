680c3fa79dfa43e75d5af6c16a44daa98a047f7e
akhalizev <ahalizev@gmail.com>
2020-12-22 01:59:12 -0600

    Destructuring

--- src/index.js
+++ src/index.js
@@ -7,23 +7,34 @@ import './index.css';
 // const Title = () => <h1>This is a title</h1>
 // const Author = () => <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop:'0.25rem', textTransform:'uppercase'}}>Author of this book</h4> //inline style
 
-const img = "https://cdn.builtinaustin.com/cdn-cgi/image/f=auto,fit=crop,sharpen=.3,w=96,h=96/sites/www.builtinaustin.com/files/2020-11/Logo_Mark_Full_Color_Web_0.png"
-const author = "Amelia Hepworth"
-const title = "I love you to the Moon and Back"
+const firstBook = {
+  img: "https://cdn.builtinaustin.com/cdn-cgi/image/f=auto,fit=crop,sharpen=.3,w=96,h=96/sites/www.builtinaustin.com/files/2020-11/Logo_Mark_Full_Color_Web_0.png",
+  author: "Amelia Hepworth",
+  title: "I love you to the Moon and Back"
+}
+const secondBook = {
+  img: "https://miro.medium.com/max/72/1*QLkYPofnRbO2xaruTjTf6g.jpeg",
+  author: "Nostos Network",
+  title: "Nostos is seeking a Member Support Lead"
+}
+// const img = "https://cdn.builtinaustin.com/cdn-cgi/image/f=auto,fit=crop,sharpen=.3,w=96,h=96/sites/www.builtinaustin.com/files/2020-11/Logo_Mark_Full_Color_Web_0.png"
+// const author = "Amelia Hepworth"
+// const title = "I love you to the Moon and Back"
 
 //components are going here, in this App called Greeting
 function BookList() {
   return (
     <section className="booklist">
-      <Book/>
-      <Book/>
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
       <Book/>
       <Book/>
     </section>
   );
 }
 
-const Book = () => {
+const Book = ({ img, title, author}) => {
+  //const {img, author, title} = props   ––– destructuring
   return <article className='book'>
     {/* <Image />
     <Title />
